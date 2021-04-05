ee52c4f820dfbcc988040ef6e473e78b9c602c39
JulianGiron <81944409+JulianGiron@users.noreply.github.com>
2021-04-04 22:13:46 -0600

    Add files via upload

---
+++ julianbloque2TAR1/src/julianbloque2tar1/Julianbloque2TAR1.java
@@ -0,0 +1,20 @@
+
+package julianbloque2tar1;
+
+
+public class Julianbloque2TAR1 {
+
+
+    public static void main(String[] args) {
+        System.out.println("Angel Julian Giron Lopez ");
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
